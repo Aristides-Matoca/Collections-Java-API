99ce8bffd600474b12f7aae82f944959c348d9de
Aristides <harry.matoca@gmail.com>
2023-09-04 18:35:53 +0100

    STreamExemplo

--- src/streamAPI/StreamExemplo.java
+++ src/streamAPI/StreamExemplo.java
@@ -1,10 +1,20 @@
 package streamAPI;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamExemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		int soma = numeros.stream()
+		                .filter(n -> n % 2 == 0) // Filtrar números pares
+		                .mapToInt(Integer::intValue) // Mapear para tipos primitivos
+		                .sum(); // Calcular a soma
+
+		System.out.println("Soma dos números pares: " + soma);
 
 	}
-
 }
